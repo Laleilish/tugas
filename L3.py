ebc516bd5e3850ad9b6faf6f28014b455c5cb4d8
Febriansyah Nugraha <nfebri622@gmail.com>
2024-11-11 08:58:34 +0700

    Add files via upload

---
+++ L3.py
@@ -0,0 +1,11 @@
+# Nama : Febriansyah Nugraha
+# NIM  : 2408163
+# Kelas: 1C
+
+n = int(input("Masukan angka:"))
+x = 1
+for i in range (n):
+    for j in range (n):
+        print (x, end=" ")
+        x +=1
+    print()
