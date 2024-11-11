94d0e5a44b5296506151534d7d35d2857ede717a
Laleilish <nfebri622@gmail.com>
2024-11-11 08:06:24 +0700

    Menambahkan file aktifitas praktikum2.py

---
+++ Aktifitas Praktikum2.py
@@ -0,0 +1,13 @@
+# Nama: Febriansyah Nugraha
+# NIM : 2408163
+# Kelas: RPL-1C
+
+def nilai_ratarata(*angka):
+    if len(angka) == 0:
+        return 0
+    total = sum(angka)
+    rata_rata = total / len(angka)
+    return (f"Nilai rata-ratanya adalah: {rata_rata}")
+
+print (nilai_ratarata(2, 3, 5, 10))
+print (nilai_ratarata(5, 10))
